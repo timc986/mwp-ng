2235322c3fe24b2cee16aa6fac79e728ea273a6b
timothy chan <timothy chan>
2019-01-07 16:16:45 +0000

    login and register with email

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      email: ['', Validators.required],
       password: ['', Validators.required]
     });
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -20,8 +20,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      email: ['', Validators.required],
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
