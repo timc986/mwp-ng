5bcbc468c328f0c3e2dfb3bed8257fb92ee244bd
timothy chan <timothy chan>
2019-01-08 11:59:13 +0000

    return token within user

--- src/app/Service/authentication.service.ts
+++ src/app/Service/authentication.service.ts
@@ -30,7 +30,7 @@ export class AuthenticationService {
       .pipe(
         map(response => {
           console.log('response: ', response);
-          if (response && response.token) {
+          if (response && response.user && response.user.token) {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
             localStorage.setItem('currentUser', JSON.stringify(response.user));
             this.currentUserSubject.next(response.user);
